12a6189d858fbbd465d96c0903bd13e9ad93c6d6
varHoudini <148201920+mdzeroseptsept@users.noreply.github.com>
2023-11-21 10:19:04 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,20 +1,31 @@
-   // Register
-
-   function submitForm() {
+function submitForm() {
     const name = document.getElementById('name').value;
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     // Perform client-side validation
-    if (username && password && name && email && username && password) {
+    if (name && username && email && password) {
         // Send data to server
         const registermessage = document.getElementById('registermessage');
-        registermessage.textContent = `Registered successfully as ${username}`;
+        registermessage.innerHTML = `Registered successfully as ${username}`;
         registermessage.style.color = 'green';
     } else {
         const registermessage = document.getElementById('registermessage');
-        registermessage.textContent = 'Please enter all values';
+        registermessage.innerHTML = 'Please enter all values';
         registermessage.style.color = 'red';
     }
-};
+}
+
+function togglePassword() {
+    var passwordInput = document.getElementById('password');
+    var showPasswordSpan = document.querySelector('.show-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        showPasswordSpan.innerHTML = '<i class="bx bx-hide"></i>';
+    } else {
+        passwordInput.type = 'password';
+        showPasswordSpan.innerHTML = '<i class="bx bx-show"></i>';
+    }
+}
